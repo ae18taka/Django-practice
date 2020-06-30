5fa60a4a8c2fcfe83bc3967c1c4a36cb46535ad8
ae18taka <ae18taka>
2020-06-30 13:35:20 +0900

    create comment function

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns =[
-    path('',views.PostList.as_view(),name='post_list')
+    path('',views.PostList.as_view(),name='post_list'),
+    path('detail/<int:pk>/',views.PostDetail.as_view(),name='post_detail'),
+    path('comment/<int:pk>',views.CommentCreate.as_view(),name='comment_create'),
 ]
